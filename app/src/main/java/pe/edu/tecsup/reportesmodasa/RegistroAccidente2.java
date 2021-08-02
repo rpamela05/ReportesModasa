3dbc64902cc658637d2f20c53ff9a40ddd06a41b
Your Name <you@example.com>
2021-08-02 15:48:45 -0500

    second commit

--- app/src/main/java/pe/edu/tecsup/reportesmodasa/RegistroAccidente2.java
+++ app/src/main/java/pe/edu/tecsup/reportesmodasa/RegistroAccidente2.java
@@ -2,14 +2,27 @@ package pe.edu.tecsup.reportesmodasa;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class RegistroAccidente2 extends AppCompatActivity {
-
+    Button btnSiguiente;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registro_accidente2);
         getSupportActionBar().hide();
+
+        btnSiguiente=findViewById(R.id.btnSiguiente);
+
+        btnSiguiente.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(),RegistroAccidente3.class));
+                finish();
+            }
+        });
     }
 }
--- app/src/main/java/pe/edu/tecsup/reportesmodasa/RegistroAccidente5.java
+++ app/src/main/java/pe/edu/tecsup/reportesmodasa/RegistroAccidente5.java
@@ -2,13 +2,27 @@ package pe.edu.tecsup.reportesmodasa;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class RegistroAccidente5 extends AppCompatActivity {
-
+    Button btnSiguiente;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registro_accidente5);
+        getSupportActionBar().hide();
+
+        btnSiguiente=findViewById(R.id.btnSiguiente);
+
+        btnSiguiente.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(),RegistroAccidente6.class));
+                finish();
+            }
+        });
     }
 }
