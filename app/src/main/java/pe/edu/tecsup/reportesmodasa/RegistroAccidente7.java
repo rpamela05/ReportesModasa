03cab94bd361f81a3d5b46000a674d0b67697873
karoline bustinza <karoline.bustinza@tecsup.edu.pe>
2021-08-13 03:05:13 -0500

    cuarto commit

--- app/src/main/java/pe/edu/tecsup/reportesmodasa/RegistroAccidente7.java
+++ app/src/main/java/pe/edu/tecsup/reportesmodasa/RegistroAccidente7.java
@@ -7,13 +7,12 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-public class RegistroAccidente6 extends  AppCompatActivity {
-
+public class RegistroAccidente7 extends AppCompatActivity {
     Button btnSiguiente;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_registro_accidente6);
+        setContentView(R.layout.activity_registro_accidente7);
         getSupportActionBar().hide();
 
         btnSiguiente=findViewById(R.id.btnSiguiente);
@@ -21,7 +20,7 @@ public class RegistroAccidente6 extends  AppCompatActivity {
         btnSiguiente.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(getApplicationContext(),RegistroAccidente7.class));
+                startActivity(new Intent(getApplicationContext(),RegistroAccidente8.class));
                 finish();
             }
         });
