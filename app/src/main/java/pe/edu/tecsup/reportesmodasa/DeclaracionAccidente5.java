2929e2ddd61725cd2cba1d16badd4617e65229e8
karoline bustinza <karoline.bustinza@tecsup.edu.pe>
2021-08-13 03:13:31 -0500

    quinto commit

---
+++ app/src/main/java/pe/edu/tecsup/reportesmodasa/DeclaracionAccidente5.java
@@ -0,0 +1,14 @@
+package pe.edu.tecsup.reportesmodasa;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DeclaracionAccidente5 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_declaracion_accidente5);
+    }
+}
